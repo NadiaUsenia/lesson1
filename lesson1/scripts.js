62284c3e70328766d994382161e9beaab65b8476
NadiaUsenia <nadzeya_usenia@mail.ru>
2021-08-20 16:48:42 +0300

    First commit

---
+++ lesson1/scripts.js
@@ -0,0 +1,79 @@
+let a1 = 1,
+    a2 = 2,
+    a3 = 3,
+    a4 = 4,
+    a5 = 5,
+    a6 = 6,
+    a7 = 7,
+    a8 = 8,
+    a9 = 9,
+    a10 = 10,
+    a11 = 11,
+    a12 = 12,
+    a13 = 13,
+    a14 = 14,
+    a15 = 15,
+    a16 = 16,
+    a17 = 17,
+    a18 = 18;
+console.log(a5%a3);
+console.log(a3%a5);
+console.log(a5+'3');
+console.log('5'-a3);
+console.log(75+'кг');
+console.log(785*653);
+console.log(100 / 25);
+console.log(0*0);
+console.log(0%2);
+console.log(89%0);
+console.log(98+2);
+console.log(5-98);
+console.log((8 + 56 * 4) / 5);
+console.log((9 - 12) * 7 / (5 + 2));
+console.log(+"123");
+console.log(1 || 0);
+console.log(false || true);
+console.log(true > 0);
+// 3
+let width = 10,
+    height = 23;
+    SPryam = width*height;
+    console.log(SPryam +'см');
+
+// 4
+let height4 = 10,
+    d4 = a7,
+    VCilindra = height4*d4;
+    console.log(VCilindra);
+
+// 5
+
+let r5 = 5,
+    p5 = 3.14,
+    SKruga = p5*r5*r5;
+    console.log(SKruga);
+// 6
+/*let height6 = 10,
+    a = a5,
+    b = a7,
+    STrap = (a+b)/2*height6;
+    console.log(STrap);*/
+// 7
+
+// 8
+let a = a8,
+    b = 3,
+    sum8_1 = ((16+2*b)-a)/2;
+    console.log(sum8_1);
+
+
+    
+
+
+    
+    
+
+
+
+
+
